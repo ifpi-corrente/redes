efbf8e3d1192586dd78dc745c15c146177d94abf
lailson henrique <lailson.pi@gmail.com>
2019-11-21 19:54:18 -0300

    criando socket servidor

--- cliente.py
+++ cliente.py
@@ -1,2 +1,10 @@
+#!/usr/bin/python3
+#Cliente UDP
 
-chest
+import socket
+
+cliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+while True:
+    mensagem_enviada = input('Digite uma mensagem: ')
+    cliente.sendto(mensagem_enviada.encode(),("10.8.19.219:"))
--- servidor.py
+++ servidor.py
@@ -1,1 +1,14 @@
-teste
+#!/usr/bin/python3
+#Servidor UDP
+
+import socket
+
+#criar um socket IPv4 e do tipo UDP
+servidor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+#Servidor vai ficar escutando nolocalhost
+#e na porta 12000
+servidor.bind(('', 13000))
+
+while True:
+    
